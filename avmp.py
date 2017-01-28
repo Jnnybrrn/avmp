46937f5cd44833510d1a629da82b6c79c0c6bada
Jonathan Barron <up661670@myport.ac.uk>
2017-01-28 19:15:11 +0000

    Add checkFileValid to validify filepath given
    Only accepts files that exist, and that we can read.

--- avmp.py
+++ avmp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import argparse, sys
+import argparse, sys, os
 
 def main():
     # Define arguments
@@ -14,13 +14,45 @@ def main():
     # Store arguments
     global verbose, filename
     verbose = args.verbose
-    filename = args.file
+    filepath = args.file
     command = args.command
 
     # Do actions based on arguments
-    print "Filename is: ", filename
-    switch(command)
-    exit(1)
+    # Filepath
+    fileValid = checkFileValid(filepath)
+
+    # Command
+    if fileValid:
+        switch(command)
+    else:
+        # Finished
+        exit(1)
+
+def checkFileValid( filepath ):
+    # print "Filepath is: ", filepath
+    # print "File is: ", os.path.basename(filepath)
+    # print "Dirname is: ", os.path.dirname(filepath)
+    if os.path.exists(filepath):
+        if os.path.isfile(filepath):
+            print "This file exists"
+            print "LastModifiedTime is: ", os.path.getmtime(filepath)
+            try:
+                with open(filepath) as tempFile:
+                    return True
+            except Exception as err:
+                print "File is not readable,", err
+                return False
+        elif os.path.isdir(filepath):
+            # TODO: Implement passing a folder (and it searching for .yaml)
+            print "Passing a folder not implemented"
+            return False
+        else:
+            print "Something went wrong; I don't know what file is."
+            return False
+    else:
+        print "File", filepath," not found (or no access). Exiting"
+        return False
+
 
 def switch( command ):
     if command == 'up':
